af7000dafb377791f78f5dfa3e6a82e9d10c1869
Rian S. Macedo <riansm100@gmail.com>
2024-12-05 07:10:05 -0300

    adicionado duas respostas por limitação de caracteres

--- Program.cs
+++ Program.cs
@@ -11,8 +11,14 @@
         string recrutador = args[0];
         string empresa = args[1];
 
-        string msg = $"Olá {recrutador},\n\nEncontrei seu perfil no LinkedIn e gostaria de me conectar com você para possiveis oportunidades. Estou buscando novas oportunidades na área de {rule} e adoraria conectar-me para aprender mais sobre as oportunidades na {empresa}.\n\nTenho {timeExperience} de experiência em desenvolvimento de software e acredito que minhas habilidades em {skills} podem ser um bom complemento para a equipe da {empresa}.\n\nAgradeço pelo seu tempo e espero que possamos nos conectar.\n\nAtenciosamente,\n{yourName}";     
-    
+        string msg = $"\n\nOlá {recrutador},\n\nEncontrei seu perfil no LinkedIn e gostaria de me conectar com você para possíveis oportunidades. Estou buscando novas oportunidades na área de {rule} e adoraria conectar-me para aprender mais sobre as oportunidades na {empresa}.\n\nTenho {timeExperience} de experiência em desenvolvimento de software e acredito que minhas habilidades em {skills} podem ser um bom complemento para a equipe da {empresa}.\n\nAgradeço pelo seu tempo e espero que possamos nos conectar.\n\nAtenciosamente,\n{yourName}";     
+        string msgResumida = $"Olá {recrutador},\nencontrei seu perfil no LinkedIn e fiquei impressionado com sua experiência. Busco novas oportunidades em {rule} e gostaria de conectar-me.\nAtenciosamente,\n{yourName}";
+        
         Console.WriteLine(msg);
+        Console.WriteLine($"\n\nQuantidade de Caracters: {msg.Length}\n\n");
+        
+        Console.WriteLine(msgResumida);
+        Console.WriteLine($"\n\nQuantidade de Caracters: {msgResumida.Length}\n\n");
+
     }
 }
